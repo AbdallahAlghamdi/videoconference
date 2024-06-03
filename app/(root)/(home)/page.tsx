af6ad88a6ab12add00517f58b28b9597bf736a9d
Abdullah Alghamdi <75899918+AbdallahAlghamdi@users.noreply.github.com>
2024-06-03 12:12:57 +0300

    Removing unneccessary toast

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,4 @@
-'use client'
 import MeetingTypeList from '@/components/MeetingTypeList';
-import { toast } from '@/components/ui/use-toast';
 import React from 'react'
 
 const Home = () => {
@@ -18,10 +16,7 @@ const Home = () => {
   return (
     <section className='flex size-full flex-col gap-10 text-white'>
       <div  className='h-[300px] md:p-5 lg:p-0  w-full rounded-[20px] bg-hero bg-cover'>
-        <div onClick={()=>toast({
-               title: ClientTime.toString(),
-               description: "Friday, February 10, 2023 at 5:57 PM",
-             })} className='flex h-full flex-col justify-end max-md:px-5 max-md:py-8 lg:p-11'>
+        <div className='flex h-full flex-col justify-end max-md:px-5 max-md:py-8 lg:p-11'>
           <div className='flex flex-col gap-2'>
             <h1 className='text-4xl items-end font-extrabold lg:text-7xl'>{TIME} </h1>
             <p className='text-lg font-medium text-sky-1 lg:text-2xl'> {DATE}</p>
