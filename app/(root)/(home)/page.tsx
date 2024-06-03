1a780b3d80bbb04cf9429eb7a1849c5fa5f12555
Abdullah Alghamdi <75899918+AbdallahAlghamdi@users.noreply.github.com>
2024-06-03 10:55:32 +0300

    Seeting the page to indicate that the time is in UTC

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -15,7 +15,7 @@ const Home = () => {
       <div className='h-[300px] md:p-5 lg:p-0  w-full rounded-[20px] bg-hero bg-cover'>
         <div className='flex h-full flex-col justify-end max-md:px-5 max-md:py-8 lg:p-11'>
           <div className='flex flex-col gap-2'>
-            <h1 className='text-4xl font-extrabold lg:text-7xl'>{TIME}</h1>
+            <h1 className='text-4xl flex flex-row items-end font-extrabold lg:text-7xl'>{TIME} <p className='ml-2 text-base'>UTC TIME</p></h1>
             <p className='text-lg font-medium text-sky-1 lg:text-2xl'> {DATE}</p>
           </div>
         </div>
